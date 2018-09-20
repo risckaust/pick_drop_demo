8484bb4ac152b6491d4ba8bbcb461c34a7a0447b
Mohamed zahana <mohamedashraf123@gmail.com>
2018-09-20 10:57:06 +0300

    modify arduino code to send button state to ROS

--- gripper_joystick/gripper_joystick.ino
+++ gripper_joystick/gripper_joystick.ino
@@ -13,6 +13,7 @@
 #include <Servo.h>
 #include <ros.h>
 #include <sensor_msgs/Joy.h>
+#include <std_msgs/Bool.h>
  
 Servo servo1; Servo servo2; // create servo objects to control the servo motors
 
@@ -25,6 +26,10 @@ int pos = 0;                   // variable to store the servo position
 
 // instantiate the node handle
 ros::NodeHandle nh;
+
+// ROS msg for button status
+std_msgs::Bool button_msg;
+ros::Publisher button_pub("gripper_status", &button_msg);
 
 // callback
 void MsgCb( const sensor_msgs::Joy& msg){
@@ -61,6 +66,14 @@ void loop(){
   buttonState = digitalRead(pushbutton);  // read the state of the pushbutton value
   nh.spinOnce();
   delay(1);
-  if (buttonState == LOW)                      // check if the pushbutton is not pressed.
+  if (buttonState == LOW){                      // check if the pushbutton is not pressed.
+    button_msg.data = true;
     nh.loginfo("Drop to pick-up again");
+  }
+  else{
+    button_msg.data = false;
+  }
+
+  button_pub.publish(&button_msg);
+  
 } 
